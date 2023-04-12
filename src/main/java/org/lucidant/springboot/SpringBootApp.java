4216497abec7c526f1a64328a19e944080ccea71
christweekie <chris.faulkner@gmail.com>
2023-04-12 16:47:30 +0100

    Add controller

---
+++ src/main/java/org/lucidant/springboot/SpringBootApp.java
@@ -0,0 +1,11 @@
+package org.lucidant.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
