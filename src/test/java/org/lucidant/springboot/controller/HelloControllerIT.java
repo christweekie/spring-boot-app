e31ad0ac823e69fee89d7a2d00a8158aab945a51
christweekie <chris.faulkner@gmail.com>
2023-04-12 16:47:30 +0100

    Add controller

---
+++ src/test/java/org/lucidant/springboot/controller/HelloControllerIT.java
@@ -0,0 +1,36 @@
+package org.lucidant.springboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HelloControllerIT {
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private HelloController controller;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(controller);
+    }
+
+    @Test
+    void givenCall_controller_thenReturnGreeting() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+            String.class)).contains("Greetings from Spring Boot!");
+    }
+}
