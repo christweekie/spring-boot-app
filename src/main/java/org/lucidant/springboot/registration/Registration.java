d3d142323bb1a7fca15bdc6d4f2e7e1cd6ac3716
christweekie <chris.faulkner@gmail.com>
2024-01-04 13:45:58 +0000

    Pluralsight course updates

---
+++ src/main/java/org/lucidant/springboot/registration/Registration.java
@@ -0,0 +1,11 @@
+package org.lucidant.springboot.registration;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record Registration(
+        Integer id,
+        @NotNull(message = "Product id is required") Integer productId,
+        String ticketCode,
+        @NotBlank(message = "Attendee name is required") String attendeeName) {
+}
